08cd38974cbb8934e374bfbf241ba12a8be73749
Tom <veniat.tom@gmail.com>
2018-12-14 19:03:58 +0100

    update external resources

--- src/utils/external_resources.py
+++ src/utils/external_resources.py
@@ -2,55 +2,70 @@ import json
 
 import gridfs
 from pymongo import MongoClient
-
-# from sacred.observers import MongoObserver
-from .observers import CustomMongoObserver
-
-MONGO_CONF_PATH = 'resources/mongo.json'
+from sacred.observers import MongoObserver
+from visdom import Visdom
 
 
-def _get_conf(mongo_path=MONGO_CONF_PATH):
-    with open(mongo_path) as file:
-        mongo_conf = json.load(file)
-    return mongo_conf
+def load_conf(path):
+    with open(path) as file:
+        conf = json.load(file)
+    return conf
 
 
-def _get_mongo_connection_url(mongo_conf):
+###
+# Mongodb
+###
+def get_mongo_connection_url(mongo_conf=None, mongo_path=None):
+    if mongo_conf is None:
+        mongo_conf = load_conf(mongo_path)
     db_user = '{}:{}'.format(mongo_conf['user'], mongo_conf['passwd'])
     db_host = '{}:{}'.format(mongo_conf['host'], mongo_conf['port'])
-    return 'mongodb://{}@{}/{}'.format(db_user, db_host, mongo_conf['auth_db'])
+    auth_db = mongo_conf.get('auth_db', mongo_conf['db'])
+    return 'mongodb://{}@{}/{}'.format(db_user, db_host, auth_db)
 
 
-def _get_db_name(debug, mongo_conf):
-    return mongo_conf['debug_db'] if debug else mongo_conf['db']
-
-
-def get_mongo_db(debug, mongo_conf=None, mongo_path=MONGO_CONF_PATH):
+def get_mongo_db(mongo_conf=None, mongo_path=None):
     if mongo_conf is None:
-        mongo_conf = _get_conf(mongo_path)
-    connection_url = _get_mongo_connection_url(mongo_conf)
-    client = MongoClient(connection_url)
-    db_name = _get_db_name(debug, mongo_conf)
-    return client[db_name]
+        mongo_conf = load_conf(mongo_path)
+
+    connection_url = get_mongo_connection_url(mongo_conf)
+    return MongoClient(connection_url)[mongo_conf['db']]
 
 
-def get_mongo_collection(debug, mongo_conf=None, mongo_path=MONGO_CONF_PATH):
+def get_mongo_collection(mongo_conf=None, mongo_path=None, collection=None):
     if mongo_conf is None:
-        mongo_conf = _get_conf(mongo_path)
-    db = get_mongo_db(debug, mongo_conf)
-    coll_name = mongo_conf['collection']
-    return db[coll_name]
+        mongo_conf = load_conf(mongo_path)
+    db = get_mongo_db(mongo_conf)
+
+    if collection is None:
+        collection = mongo_conf['collection']
+
+    return db[collection]
 
 
-def get_gridfs(debug, mongo_conf=None):
+def get_mongo_obs(mongo_conf=None, mongo_path=None):
     if mongo_conf is None:
-        mongo_conf = _get_conf()
-    return gridfs.GridFS(get_mongo_db(debug, mongo_conf))
+        mongo_conf = load_conf(mongo_path)
+
+    db_url = get_mongo_connection_url(mongo_conf)
+    return MongoObserver.create(url=db_url, db_name=mongo_conf['db'], collection=mongo_conf['collection'])
 
 
-def get_mongo_obs(debug, mongo_conf=None):
+def get_gridfs(mongo_conf=None, mongo_path=None):
     if mongo_conf is None:
-        mongo_conf = _get_conf()
-    db_url = _get_mongo_connection_url(mongo_conf)
-    db_name = _get_db_name(debug, mongo_conf)
-    return CustomMongoObserver.create(url=db_url, db_name=db_name, collection=mongo_conf['collection'])
+        mongo_conf = load_conf(mongo_path)
+
+    return gridfs.GridFS(get_mongo_db(mongo_conf))
+
+
+###
+# Visdom
+###
+def get_visdom(visdom_path=None, **conf_updates):
+    visdom_conf = dict(raise_exceptions=True)
+
+    if visdom_path is not None:
+        visdom_conf.update(load_conf(visdom_path))
+
+    visdom_conf.update(conf_updates)
+    return Visdom(**visdom_conf)
